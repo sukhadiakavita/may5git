487954eb414bca90c2b5bd024894a6bae73196af
sukhadiakavita <122067477+sukhadiakavita@users.noreply.github.com>
2023-05-08 22:17:13 -0500

    Initial file push

---
+++ google/src/test/java/test/com/mycompany/GoogleTest.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package test.com.mycompany;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author karma
+ */
+public class GoogleTest {
+     private WebDriver driver;
+    public GoogleTest() {
+        
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+    @Test
+  public void testGoogle() throws Exception {
+    driver.get("https://www.google.com/");
+    driver.findElement(By.id("APjFqb")).click();
+  }
+}
